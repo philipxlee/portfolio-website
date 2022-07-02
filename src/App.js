ef4c3caefb66ba93f18466b1eb12629e3322f125
philipxlee <philipleektx@gmail.com>
2022-07-02 20:44:54 +0800

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/portfolio-website" element={<Home />} />
         <Route exact path="/projects" element={<Projects />} />
         <Route exact path="/experience" element={<Experience />} />
       </Routes>
